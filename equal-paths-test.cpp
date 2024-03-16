cbc3ae2eeaf6d7e2b02daf97e5609113ba0ab27b
Sonia Savur <ssavur@usc.edu>
2024-03-16 18:26:42 +0000

    finished tree traversals part

--- equal-paths-test.cpp
+++ equal-paths-test.cpp
@@ -10,6 +10,9 @@ Node* c;
 Node* d;
 Node* e;
 Node* f;
+Node* g;
+Node* h;
+Node* i;
 
 void setNode(Node* n, int key, Node* left=NULL, Node* right=NULL)
 {
@@ -55,6 +58,20 @@ void test5(const char* msg)
   cout << msg << ": " <<   equalPaths(a) << endl;
 }
 
+void falsetest3(const char* msg)
+{
+  setNode(a, 1, NULL, b);
+  setNode(b, 3, c, d);
+  setNode(c, 2, NULL, NULL);
+  setNode(d, 8, e, f);
+  setNode(e, 6, g, h);
+  setNode(f, 9, NULL, NULL);
+  setNode(g, 5, i, NULL);
+  setNode(h, 7, NULL, NULL);
+  setNode(i, 4, NULL, NULL);
+  cout << msg << ": " <<   equalPaths(a) << endl;
+}
+
 int main()
 {
   a = new Node(1);
@@ -72,5 +89,28 @@ int main()
   delete b;
   delete c;
   delete d;
+
+  // FalseTest3
+  a = new Node(1);
+  b = new Node(3);
+  c = new Node(2);
+  d = new Node(8);
+  e = new Node(6);
+  f = new Node(9);
+  g = new Node(5);
+  h = new Node(7);
+  i = new Node(4);
+
+  falsetest3("FalseTest3");
+
+  delete a;
+  delete b;
+  delete c;
+  delete d;
+  delete e;
+  delete f;
+  delete g;
+  delete h;
+  delete i;
 }
 
--- equal-paths.cpp
+++ equal-paths.cpp
@@ -9,10 +9,95 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+// helper function to determine if node is a leaf node
+bool isLeaf(Node* n) {
+  if(n == NULL) {
+    return false;
+  }
+  else {
+    return ((n->left == NULL) && (n->right == NULL));
+  }
+}
+
+// helper function to determine if node has 2 children
+bool hasTwoChildren(Node* n) {
+  if(n == NULL) {
+    return false;
+  }
+  else {
+    return ((n->left != NULL) && (n->right != NULL)); 
+  }
+}
+
+// helper function to return length of path from node n to leaf
+int distToLeaf(Node* n) {
+  if(n == NULL) {
+    return 0;
+  }
+  else if(isLeaf(n)) {
+    return 1;
+  }
+  else if(n->left != NULL) {
+    return distToLeaf(n->left) + 1;
+  }
+  else {
+    return distToLeaf(n->right) + 1;
+  }
+}
+
+// helper function to return child of single parent
+Node* aliveChild(Node* root) {
+  if(root->left == NULL) {
+    return root->right;
+  }
+  else return root->left;
+}
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
+    // called on tree with 0 or 1 nodes: return true
+    if(isLeaf(root) || (root == NULL)) {
+        return true;
+    }
 
+    if(!hasTwoChildren(root)) {
+      Node* child = aliveChild(root);
+
+      // called on tree with only 2 nodes: return true
+      if(isLeaf(child)) {
+        return true;
+      }
+      
+      else return equalPaths(child);
+    }
+    
+    // head recursion: recursive calls to smaller binary trees
+    if(hasTwoChildren(root->left)) { 
+      if(!equalPaths(root->left)) {
+        return false;
+      }
+    }
+    if(hasTwoChildren(root->right)) { 
+      if(!equalPaths(root->right)) {
+        return false;
+      }
+    }
+
+    // evaluate this binary tree
+    if(hasTwoChildren(root)) {
+      // one child is a leaf node and the other child is not
+      if(isLeaf(root->left) != isLeaf(root->right)) {
+        return false;
+      }
+
+      // both children are or aren't leaf nodes
+      else {
+        if(distToLeaf(root->left) == distToLeaf(root->right)) {
+          return true;
+        }
+      }
+    }
+    
+    return false;
 }
 
